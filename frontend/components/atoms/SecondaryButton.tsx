f5e513e556f9d764a8e833e42b705ab2d22bbb62
klimondra <klimondra@seznam.cz>
2025-10-15 19:30:18 +0200

    feature(frontend): Added background
    Took 42 minutes

--- frontend/components/atoms/SecondaryButton.tsx
+++ frontend/components/atoms/SecondaryButton.tsx
@@ -10,7 +10,7 @@ const SecondaryButton = (props: {
         <button
             className={"py-1.25 px-3 flex items-center gap-1 " +
                 "bg-secondary text-primary font-semibold rounded-xl border-1 border-primary/30 " +
-                "transition-all duration-200 " + (props.className || "")}
+                "hover:bg-primary hover:text-secondary transition-all duration-200 " + (props.className || "")}
             onClick={props.onClick}
         >
             { props.icon && <Icon icon={props.icon} className="text-xl" /> }
