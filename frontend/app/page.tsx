b2acc38743556af13eb207dcbac0ace4783be6b9
klimondra <klimondra@seznam.cz>
2025-10-15 17:09:17 +0200

    chore(frontend): tidied up global.css
    Took 19 minutes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Menu from "@/components/composites/menu";
 
 export default function Home() {
--- frontend/components/composites/menu.tsx
+++ frontend/components/composites/menu.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Menu = () => {
     return (
         <header className={"flex justify-center px-8 py-2"}>
-            <div className={"bg-primary rounded-2xl w-11/12 h-16 px-6"}>
+            <div className={"bg-primary rounded-2xl w-11/12 h-14 px-6"}>
                 <div className={"flex items-center w-full h-full"}>
                     <Image src={"/logo/logo-light.svg"} alt={"Logo projektu světlé"} width={200} height={100}/>
                     <h2 className={"font-semibold text-primary text-lg"}>Nejnovější</h2>
